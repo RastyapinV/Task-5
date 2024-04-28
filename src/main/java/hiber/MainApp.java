a6e7167dcb34f2a61781005fffd9f3b8fe3b035c
RastyapinV <rastyapin.vova@yandex.ru>
2024-04-28 13:34:04 +0300

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,53 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+//      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("Toyota RAV4", 4)));
+//      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("Renault Logan", 3)));
+//
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         if (user.getCar() != null) {
+//            System.out.println("Car:");
+//            System.out.println("    Model = " + user.getCar().getModel());
+//            System.out.println("    Series = " + user.getCar().getSeries());
+//         }
+//         System.out.println();
+//      }
+
+        User user = userService.findOwner("Toyota RAV4", 4);
+        if (user != null) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car:");
+            System.out.println("    Model = " + user.getCar().getModel());
+            System.out.println("    Series = " + user.getCar().getSeries());
+        } else System.out.println("Для данного ТС не нашлось владельца.");
+
+        context.close();
+    }
+}
